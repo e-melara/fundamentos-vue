36db5d29237424a56c6997ca8681ffc7f0bbba12
Edwin Melara Landaverde <edwin.melara.landaverde@gmail.com>
2022-05-02 16:41:40 -0600

    Add Indecision

---
+++ src/components/Indecision.vue
@@ -0,0 +1,92 @@
+<template>
+  <img :src="img" alt="bg" />
+  <div class="bg-dark"></div>
+  <div class="indecision-container">
+    <input v-model="question" type="text" placeholder="Hazme una pregunta" />
+    <p>Recuerda terminar con un signo de interrogacion (?)</p>
+    <div v-if="isValidQuestion">
+      <h2>{{ question }}</h2>
+      <h1>{{ answer }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      question: '',
+      answer: null,
+      img: 'https://via.placeholder.com/250x250',
+      isValidQuestion: false,
+    }
+  },
+  methods: {
+    async getAnsewer() {
+      this.answer = 'Pensado ....'
+      const { answer, image } = await fetch('https://yesno.wtf/api').then((r) =>
+        r.json()
+      )
+      this.answer = answer === 'yes' ? 'Si' : 'No'
+      this.img = image
+    },
+  },
+  watch: {
+    question(newQuestion, oldValue) {
+      this.isValidQuestion = false
+      if (newQuestion.includes('?')) {
+        this.getAnsewer()
+        this.isValidQuestion = true
+      }
+      return
+    },
+  },
+}
+</script>
+
+<style>
+img,
+.bg-dark {
+  height: 100vh;
+  left: 0px;
+  max-height: 100%;
+  max-width: 100%;
+  position: fixed;
+  top: 0px;
+  width: 100vw;
+}
+
+.bg-dark {
+  background-color: rgba(0, 0, 0, 0.4);
+}
+
+.indecision-container {
+  position: relative;
+  z-index: 99;
+}
+
+input {
+  width: 250px;
+  padding: 10px 15px;
+  border-radius: 5px;
+  border: none;
+}
+input:focus {
+  outline: none;
+}
+
+p {
+  color: white;
+  font-size: 20px;
+  margin-top: 0px;
+}
+
+h1,
+h2 {
+  color: white;
+}
+
+h2 {
+  margin-top: 150px;
+}
+</style>
